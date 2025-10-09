42d8930e001ab6f5a81ae18d87740a0f35545419
JR <joserobertomedeiros810@gmail.com>
2025-10-09 14:04:58 -0300

    ok

--- models/userModel.js
+++ models/userModel.js
@@ -16,18 +16,19 @@ module.exports = {
     },
     // CRUD
     // Função para cadastrar um novo usuario
-    salvar: ({ usuario, email, senha }) => {
+    salvar: ({ usuario, email, senha, tipo }) => {
         const novoUsuario = {
             id: listaUsuarios.length + 1,
             usuario,
             email,
-            senha
+            senha,
+            tipo
         }
         listaUsuarios.push(novoUsuario)
         console.log("Novo usuário salvo:", novoUsuario);
-        return novoUsuario
-
+        return novoUsuario;
     },
+    
     // Busca todos os usuários do banco
     listarTodos: () => {
         return listaUsuarios
